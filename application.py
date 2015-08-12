92166267cec47d6d0b377d0857f33973a62b19be
Matthew R. Laue <matt@akirisolutions.com>
2015-08-12 10:28:01 -0700

    Preserve the exception traceback for BaseApp and slack the context on errors.

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import traceback
 from webob import exc
 
 from akiri.framework import GenericWSGIApplication
@@ -7,11 +8,12 @@ from slack_api import SlackAPI
 class BaseApp(GenericWSGIApplication):
     """Base class for all licensing WSGI application."""
 
+    # Blank 'raise' raises the last exception and preserves the traceback.
     def service(self, req):
         try:
             return GenericWSGIApplication.service(self, req)
         except exc.HTTPException as error:
-            raise error
+            raise
         except StandardError as error:
-            SlackAPI.error(str(error))
-            raise error
+            SlackAPI.error(str(error) + '\n' + traceback.format_exc())
+            raise
